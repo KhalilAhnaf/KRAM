d74232e78e766c6a49609ed305ae4877404a62f5
TrippyHippy <spidermanalc@gmail.com>
2024-01-25 17:47:16 -0600

    Base Game Working

--- Fitts Law.py
+++ Fitts Law.py
@@ -9,7 +9,7 @@ from datetime import timedelta
 
 
 # Constants
-WIDTH, HEIGHT = 1000, 800  # Adjusted window size
+WIDTH, HEIGHT = 800, 800  # Adjusted window size
 FPS = 60
 data = {}
 data['fittslaw'] = [] 
@@ -19,10 +19,8 @@ pygame.display.set_caption("Fitts' Law Experiment")
 pygame.mouse.set_cursor(*pygame.cursors.broken_x)
 succ_score = 0
 unsucc_score = 0
-xpos = 200
-ypos = 200
-# Change radius to change the width of the ball
-# radius = 10
+xpos = WIDTH/2
+ypos = HEIGHT/2
 
 oldx = 0
 oldy = 0
@@ -33,9 +31,10 @@ NEON_BLUE = (20, 57, 255)
 NEON_PURPLE = (128, 0, 128)
 
 # Task parameters
+circle_clicked = False
 target_sizes = [10, 20, 40, 60]
-target_distances = [(100, 200), (200, 400), (300, 600), (400, 800)]  # Adjusted positions based on your 'left' and 'right'
-positions = ['left', 'right']
+target_distances = [(0, HEIGHT/2), (100, HEIGHT/2), (200, HEIGHT/2), (300, HEIGHT/2),(500, HEIGHT/2), (600, HEIGHT/2), (700, HEIGHT/2), (800, HEIGHT/2)]
+positions = ['left', 'right'] #Un-used
 
 # Set initial position
 pos = 0
@@ -48,27 +47,40 @@ def calcDist(x1, y1, x2, y2):
 def printstmt(succ, unsucc):
     print('Successful clicks:' + str(succ) + ' vs Unsuccessful clicks:' + str(unsucc))
 
-while True:
-    pygame.display.update(); scr.fill(BLACK)
-    pygame.draw.circle(scr, NEON_PURPLE, (xpos, ypos), random.choice(target_sizes))
-    start_time = datetime.datetime.now()
-    x = pygame.mouse.get_pos()[0]
-    y = pygame.mouse.get_pos()[1]
+radius = 50
+position = WIDTH/2, HEIGHT/2
 
-    sqx = (x - xpos)**2
-    sqy = (y - ypos)**2
+def nextRad():
+    radius = random.choice(target_sizes)
+    return radius
+
+def nextPos():
+    position = random.choice(target_distances)
+    return position
+
+clock = pygame.time.Clock()
+
+while True:
+    scr.fill(BLACK)  # Clear the screen once per frame
+    pygame.draw.circle(scr, NEON_PURPLE, position, radius)
+    start_time = datetime.datetime.now()
+
     for event in pygame.event.get():
         if event.type == QUIT:
             with open('data.json', 'w') as outfile:
                 json.dump(data, outfile)
             pygame.quit()
             sys.exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            dist1 = math.sqrt(sqx + sqy)
+
+        if event.type == pygame.MOUSEBUTTONDOWN and not circle_clicked:
+            x, y = pygame.mouse.get_pos()
+
+            dist1 = math.sqrt((x - position[0])**2 + (y - position[1])**2)
             if dist1 < radius:
                 difftime = datetime.datetime.now() - start_time
                 print(str(difftime.microseconds))
                 succ_score += 1
+                circle_clicked = True
                 dist = calcDist(oldx, oldy, xpos, ypos)
 
                 # appending to data
@@ -79,10 +91,11 @@ while True:
                 })
                 print(data)
                 print("distance by click:" + str(dist1))
-                pygame.mouse.set_pos(350, 350)
                 oldx = xpos
-                oldy = ypos 
-                xpos, ypos = target_distances[pos]  # Updated to use the adjusted positions
+                oldy = ypos
+                xpos, ypos = nextPos()
+                position = xpos, ypos
+                radius = nextRad()
                 print("target size:" + str(radius))
                 print("target distances:" + str(target_distances[pos]))
                 printstmt(succ_score, unsucc_score)
@@ -92,3 +105,9 @@ while True:
             else:
                 unsucc_score += 1
                 printstmt(succ_score, unsucc_score)
+
+        elif event.type == pygame.MOUSEBUTTONUP:
+            circle_clicked = False
+
+    pygame.display.flip()  # Update the entire display
+    clock.tick(FPS)  # Cap the frame rate to the specified FPS
